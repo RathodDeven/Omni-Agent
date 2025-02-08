5900ba0205e0e0f88809498a0a4cb1dc16829a26
RathodDeven <devenrathodrd@gmail.com>
2025-02-08 17:35:22 +0000

    Integrate React Query into OnchainKitProviderWrapper for improved data fetching

--- apps/web/src/components/wrappers/OnchainKitProviderWrapper.tsx
+++ apps/web/src/components/wrappers/OnchainKitProviderWrapper.tsx
@@ -5,6 +5,7 @@ import { base } from 'wagmi/chains'
 import { OnchainKitProvider } from '@coinbase/onchainkit'
 import React, { ReactNode } from 'react'
 import { coinbaseWallet } from 'wagmi/connectors'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
 
 const wagmiConfig = createConfig({
   chains: [base],
@@ -19,14 +20,21 @@ const wagmiConfig = createConfig({
   }
 })
 
+// Create a client
+const queryClient = new QueryClient()
+
 const OnchainKitProviderWrapper = (props: { children: ReactNode }) => {
   return (
-    <OnchainKitProvider
-      apiKey={process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY}
-      chain={base} // add baseSepolia for testing
-    >
-      <WagmiProvider config={wagmiConfig}>{props.children}</WagmiProvider>
-    </OnchainKitProvider>
+    <WagmiProvider config={wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+        <OnchainKitProvider
+          apiKey={process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY}
+          chain={base} // add baseSepolia for testing
+        >
+          {props.children}
+        </OnchainKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
   )
 }
 
