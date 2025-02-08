0bb58b155cf2e99c338059ee9f0eca5fdd1256ac
RathodDeven <devenrathodrd@gmail.com>
2025-02-08 17:49:37 +0000

    Refactor OnchainKitProviderWrapper to use default configuration and improve readability

--- apps/web/src/components/wrappers/OnchainKitProviderWrapper.tsx
+++ apps/web/src/components/wrappers/OnchainKitProviderWrapper.tsx
@@ -6,18 +6,27 @@ import { OnchainKitProvider } from '@coinbase/onchainkit'
 import React, { ReactNode } from 'react'
 import { coinbaseWallet } from 'wagmi/connectors'
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+import { APP_NAME } from '../../utils/config'
+import { getDefaultConfig } from '@rainbow-me/rainbowkit'
 
-const wagmiConfig = createConfig({
-  chains: [base],
+const defaultChains = [base]
+
+const defaultTransports = {
+  [base.id]: http()
+}
+
+const config = getDefaultConfig({
+  appName: APP_NAME,
+  projectId: String(process.env.NEXT_PUBLIC_RAINBOW_KIT_PROJECT_ID),
+  // @ts-ignore
+  chains: defaultChains,
+  transports: defaultTransports,
   connectors: [
     coinbaseWallet({
       appName: 'onchainkit'
     })
   ],
-  ssr: true,
-  transports: {
-    [base.id]: http()
-  }
+  ssr: true
 })
 
 // Create a client
@@ -25,7 +34,7 @@ const queryClient = new QueryClient()
 
 const OnchainKitProviderWrapper = (props: { children: ReactNode }) => {
   return (
-    <WagmiProvider config={wagmiConfig}>
+    <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
         <OnchainKitProvider
           apiKey={process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY}
